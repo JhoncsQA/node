5c5434c41e85b764e875686c84599d595bf49998
JhoncsQA <jhoncabezas@unicomfacauca.edu.co>
2024-09-19 20:52:06 -0500

    create setTimeout

--- node/src/promises.ts
+++ node/src/promises.ts
@@ -1,4 +1,12 @@
 console.log("Inicio");
 
+    new Promise((resolve)=>{
+
+        setTimeout(()=>{
+            resolve('promesa cumplida');
+        },1000);
+
+    }).then((mensaje)=> console.log(mensaje))
+
 
 console.log("Fin");
