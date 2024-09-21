2dd4cb3143ef03c66e622cdef149f52a2575b98e
JhoncsQA <jhoncabezas@unicomfacauca.edu.co>
2024-09-20 20:17:38 -0500

    numPos Operado Ternario

--- node/src/numPos.ts
+++ node/src/numPos.ts
@@ -2,11 +2,11 @@ export function numPositivo(num1: number, num2: number) {
 
     new Promise((resolve, reject) => {
 
-        if (num1 > 0 && num2 > 0) {
-            resolve('promesa cumplida numeros positivos');
-        } else {
-            reject('promesa no cumplida no son numeros positivos');
-        }
+        num1 > 0 && num2 > 0 ? resolve('promesa cumplida numeros positivos') :
+         num1 < 0 && num2 < 0 ? reject('promesa no cumplida no son numeros positivos'):
+         'Study more!!!';
+
+    
 
     }).then((mensaje) => console.log(mensaje))
         .catch((mensajeError) => console.error(mensajeError))
